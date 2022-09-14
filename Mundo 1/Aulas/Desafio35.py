910c0cbd03e2853304e3c96a69a1f16d08bc6d33
Eduardo Pugliesi <pugliesiedu@gmail.com>
2022-09-14 11:35:30 -0300

    14/09 - Mundo 1 concluído.

--- Mundo 1/Aulas/Desafio35.py
+++ Mundo 1/Aulas/Desafio35.py
@@ -7,4 +7,9 @@ reta1 = float(input('Reta 1: '))
 reta2 = float(input('Reta 2: '))
 reta3 = float(input('Reta 3: '))
 
-print(reta1, reta2, reta3)
+#print(reta1, reta2, reta3)
+
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print('{}, as retas que você inseriu PODEM formar um triângulo!'.format(nome))
+else:
+    print('{}, as retas que você inseriu NÃO PODEM formar um triângulo!'.format(nome))
