64d4a2aed26978dc905b5ce8f6619f9dd0cf5bd1
Eduardo Pugliesi <pugliesiedu@gmail.com>
2022-09-15 13:43:05 -0300

    Ex 36 feito

--- Mundo 2/Desafio36.py
+++ Mundo 2/Desafio36.py
@@ -2,3 +2,17 @@
 #O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar
 
 #Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salaŕio ou então o empréstimo será negado
+
+nome = str(input('Qual o seu nome? '))
+casa = float(input('Qual o valor da casa? '))
+salario = float(input('Qual o seu salário? '))
+anos = int(input('Quer financiar em quantos anos? '))
+meses = anos * 12
+parcelas = casa / meses
+
+parcelaMaxima = salario * 30 / 100
+
+if parcelas > parcelaMaxima:
+    print('{}, o valor da parcela supera em 30% do seu salário. Financiamento negado!'.format(nome))
+if parcelas <= parcelaMaxima:
+    print('{} financiamento aprovado! Você pagará R${:.2f} por mês durante {} meses'.format(nome, parcelas, meses))
