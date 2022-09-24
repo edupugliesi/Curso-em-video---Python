5d5d8f149a4489650c3bb05d318ccfb8095bc9fb
Eduardo Pugliesi <pugliesiedu@gmail.com>
2022-09-24 09:38:22 -0300

    24/09 - Mundo 2 Concluído!

--- Mundo 2/Desafio71.py
+++ Mundo 2/Desafio71.py
@@ -4,3 +4,29 @@ No início, pergunte ao usuário qual será o valor a ser sacado (Numero inteiro
 quantas cédulas de cada valor serão entregues.
 
 OBS: Considera que o caixa possui cédulas de R$50, R$20, R$10, R$1'''
+
+print('='*30)
+
+valor = int(input('Quanto você quer sacar? R$: '))
+total = valor
+ced = 50
+totalced = 0
+
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0: 
+            break
+print('='*30)
+print('Obrigado e volte sempre!')
