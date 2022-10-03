9d48e4ea21759156c071f1114bfc4fea7bf631b2
Eduardo Pugliesi <pugliesiedu@gmail.com>
2022-10-03 12:56:40 -0300

    03/10 - Desafios 78 ao 83 concluídos

--- Mundo 3/Desafio83.py
+++ Mundo 3/Desafio83.py
@@ -7,14 +7,21 @@ expressao = str(input('Digite a expressão: '))
 parentesesAberto = []
 parentesesFechado = []
 
+# Procura os parênteses existentes na expressão
 for parenteses in expressao:
+    # Se encontrar um parênteses abrindo, adiciona um parêntes aberto na lista parentesesAberto = []
     if parenteses == '(':
         parentesesAberto.append('(')
+        
+    # Se encontrar um parênteses fechando, adiciomna um parênteses fechado na lista parentesesFechado = []
     elif parenteses == ')':
         parentesesFechado.append(')')
-    
+
+# IF para comparar se a quantidade de parêntes abertos e fechados nas respectivas listas são iguais.
 if len(parentesesAberto) == len(parentesesFechado):
     print('Sua expressão está correta!')
+
+# IF para comparar se a quantidade de parêntes abertos e fechados nas respectivas listas são diferentes.
 elif len(parentesesAberto) != len(parentesesFechado):
     print('Sua expressão está errada!')
         
