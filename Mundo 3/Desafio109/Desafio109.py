edf093174243d40e41074f159b81611f5975392b
Eduardo Pugliesi <pugliesiedu@gmail.com>
2022-11-08 11:35:45 -0300

    08/11 - Desafio109 concluído

--- Mundo 3/Desafio109/Desafio109.py
+++ Mundo 3/Desafio109/Desafio109.py
@@ -1,4 +1,17 @@
 '''
 Modifique as funções que foram criadas no desafio107 para que elas aceitem um parâmetro a mais,
 informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida
-no desafio108'''
+no desafio108
+'''
+
+from Modulos109 import moeda, titulo
+
+titulo.titulo('Desafio107 - Manipulação de valores')
+
+valor = float(input('Digite um valor (R$): '))
+taxa = int(input('Digite a taxa (%): '))
+
+print(f'O valor aumentado em {taxa}% é {moeda.moeda(moeda.aumentar(valor, taxa), format=True)}')
+print(f'O valor diminuído em {taxa}% é {moeda.moeda(moeda.diminuir(valor, taxa), format=True)}')
+print(f'O dobro de {moeda.moeda(valor)} é {moeda.moeda(moeda.dobro(valor), format=True)}')
+print(f'A metade de {moeda.moeda(valor)} é {moeda.moeda(moeda.metade(valor), format=True)}')
---
+++ Mundo 3/Desafio109/Modulos109/moeda.py
@@ -0,0 +1,23 @@
+def aumentar(p = 0, t = 0):
+    final = p + (p * t/100)
+    return final
+
+def diminuir(p = 0, t = 0):
+    final = p - (p * t/100)
+    return final
+
+def dobro(p = 0):
+    final = p * 2
+    return final
+
+def metade(p = 0):
+    final = p / 2
+    return final
+
+def moeda(preço = 0, moeda = '', format=False):
+    if format == True:
+        moeda = 'R$'
+        return f'{moeda}{preço:.2f}'.replace('.', ',')
+    else:
+        return f'{moeda}{preço}'
+    
